1148067e012945abcb6a4478d05022e49e9a0988
anarcher <anarcher@gmail.com>
2016-12-08 21:51:07 +0900

    Add instance plugin interface's impls.

--- cmd/instance-gcp/main.go
+++ cmd/instance-gcp/main.go
@@ -4,10 +4,13 @@ import (
 	"os"
 
 	log "github.com/Sirupsen/logrus"
-	//"github.com/anarcher/infrakit.gcp/plugin/instance"
+	"github.com/anarcher/infrakit.gcp/plugin/instance"
 	"github.com/docker/infrakit/pkg/cli"
-	//instance_plugin "github.com/docker/infrakit/pkg/rpc/instance"
+	instance_plugin "github.com/docker/infrakit/pkg/rpc/instance"
 	"github.com/spf13/cobra"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/compute/v1"
 	//"strings"
 )
 
@@ -17,7 +20,24 @@ func main() {
 	var name string
 	var namespaceTags []string
 
-	cmd := &cobra.Command{}
+	cmd := &cobra.Command{
+		Use:   os.Args[0],
+		Short: "GCE instance plugin",
+		Run: func(c *cobra.Command, args []string) {
+
+			ctx := context.Background()
+
+			client, err := google.DefaultClient(ctx, compute.ComputeScope)
+			if err != nil {
+				log.Error(err)
+				os.Exit(1)
+			}
+
+			instancePlugin := instance.NewInstancePlugin(client)
+			cli.SetLogLevel(logLevel)
+			cli.RunPlugin(name, instance_plugin.PluginServer(instancePlugin))
+		},
+	}
 
 	cmd.Flags().IntVar(&logLevel, "log", cli.DefaultLogLevel, "Logging level. 0 is least verbose. Max is 5")
 	cmd.Flags().StringVar(&name, "name", "instance-gcp", "Plugin name to advertise for discovery")
